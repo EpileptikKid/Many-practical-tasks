3e615d8151052217bc3e3416a2706565ee53a428
EpileptikKid <k1d.ep1lept1k@gmail.com>
2022-06-08 23:19:49 +0300

    1 commit

--- task_2.java
+++ task_2.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 public class task_2 {
+    public static double doubletwo(double eps){
+        double x = 1;
+        double result = 0;
+        while (Math.abs(x) > eps) {
+            result += x;
+            x /= 2;
+        }
+        return(result);
+    }
     public static void main(String[] args){
-        int x = 2;
-        System.out.println(1 / 2 << 3);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите eps");
+        double eps = scanner.nextDouble();
+        System.out.println(task_2.doubletwo(eps));
     }
 }
