80dd46ec11e3672677ae6ea7e189c5dd6d025eb3
EpileptikKid <k1d.ep1lept1k@gmail.com>
2022-06-08 19:34:38 +0300

    1 commit

--- App.java
+++ App.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 public class App {
-    public static void main(String[] args){
-        System.out.println("Hello_Vitalik");
-        System.out.println("1");
-        System.out.println("change");
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите n в диапазоне от 0 до 30");
+        int n = scanner.nextInt();
+        if (n < 0 || n > 30) {
+            System.err.println("Неправильное значение n!");
+        }
+        else {
+            for (int i = 0; i <= n; i++) {
+                System.out.printf("2 ^ %2d = %d\n", i, 1 << i);
+            }
+        }
     }
 }
