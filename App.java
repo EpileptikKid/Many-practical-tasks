4e439b00560bf478deccf43c26fb485ed6889c3a
EpileptikKid <k1d.ep1lept1k@gmail.com>
2022-06-08 23:01:33 +0300

    1 commit

--- App.java
+++ App.java
@@ -3,15 +3,26 @@ public class App {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите n в диапазоне от 0 до 30");
-        int n = scanner.nextInt();
-        if (n < 0 || n > 30) {
-            System.err.println("Неправильное значение n!");
+        System.out.println("Квадратное уравнение в виде ax2 + bx + c = 0");
+        double a, b, c, d;
+        System.out.println("Введите a");
+        a = scanner.nextFloat();
+        System.out.println("Введите b");
+        b = scanner.nextFloat();
+        System.out.println("Введите c");
+        c = scanner.nextFloat();
+        d = Math.pow(b, 2) - (4 * a * c);
+        double x1, x2;
+        if (d>0){
+            x1 = (-b - Math.sqrt(d)) / (2 * a);
+            x2 = (-b + Math.sqrt(d)) / (2 * a);
+            System.out.printf("x1 = %f\n x2 = %f", x1, x2);
+        } else if (d==0){
+            x1 = -b / (2 * a);
+            System.out.printf("x1,2 = %f\n", x1);
+        } else {
+            System.out.println("корней нет");
         }
-        else {
-            for (int i = 0; i <= n; i++) {
-                System.out.printf("2 ^ %2d = %d\n", i, 1 << i);
-            }
+
         }
     }
-}
---
+++ task_2.java
@@ -0,0 +1,6 @@
+public class task_2 {
+    public static void main(String[] args){
+        int x = 2;
+        System.out.println(1 / 2 << 3);
+    }
+}
