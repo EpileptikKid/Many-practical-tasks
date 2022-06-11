43afe0b00cb22e0e83c3e16f5e2ec95052d53c65
EpileptikKid <k1d.ep1lept1k@gmail.com>
2022-06-11 22:00:42 +0300

    1 commit

---
+++ task_4_22.java
@@ -0,0 +1,30 @@
+//Ввести с клавиатуры строку программного кода. Проверить соответствие фигурных скобок
+//Заменить открывающиеся фигурные скобки словом begin, а закрывающиеся – словом end. Использовать класс StringBuilder.
+
+import java.util.Scanner;
+
+public class task_4_22 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку программного кода");
+        String str = scanner.nextLine();
+        StringBuilder sbStr = new StringBuilder(str);
+        int beg = 0, end = 0;
+        while ((sbStr.indexOf("{") != -1) || (sbStr.indexOf("}") != -1)){
+            if (sbStr.indexOf("{") != -1){
+                sbStr.replace(sbStr.indexOf("{"), sbStr.indexOf("{") + 1, "\nbegin\n");
+                beg++;
+            }
+            if (sbStr.indexOf("}") != -1){
+                sbStr.replace(sbStr.indexOf("}"), sbStr.indexOf("}") + 1, "\nend");
+                end++;
+            }
+        }
+        if (beg == end) {
+            String result = sbStr.toString();
+            System.out.println(result);
+        } else {
+            System.out.println("error");
+        }
+    }
+}
