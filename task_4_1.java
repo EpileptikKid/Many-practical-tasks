ccb8d8cb4e358b79fe614f2647568268eb89bc97
Andrii <107118319+EpileptikKid@users.noreply.github.com>
2022-09-17 19:50:05 +0300

    Update task_4_1.java

--- task_4_1.java
+++ task_4_1.java
@@ -1,48 +1,36 @@
-//Проинициализировать одномерный массив вещественных чисел списком начальных значений.
-//Найти сумму положительных элементов. Реализовать два варианта – с циклом и с рекурсией.
+//Enter a string from the keyboard. Remove extra spaces, including leading and trailing spaces
+//Replace capital letters with small letters inside words
+//If there is no period before words beginning with a capital letter, add a period at the end of preceding words
+//Form and display words that begin with a soft sign
 
 import java.util.Scanner;
 
-public class task_4_1 {
+public class task_4_20 {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the string:");
+            String str = scanner.nextLine();
+            str = str.trim();
+            str = str.replaceAll("\\s+", " ");
 
-    public static float sumMass(float[] arr){
-        float sum = 0;
-        for (int i = 0; i < arr.length; i++){
-            if (arr[i] > 0){
-                sum += arr[i];
+            String[] strArr = str.split(" ");
+
+            for (int i = 0; i < strArr.length; i++) {
+                strArr[i] = strArr[i].charAt(0) + strArr[i].toLowerCase().substring(1);
             }
+            for (int i = 1; i < strArr.length; i++) {
+                if (!strArr[i].equals(strArr[i].toLowerCase()) & !strArr[i - 1].endsWith(".")) {
+                    strArr[i - 1] = strArr[i - 1].concat(".");
+                }
+            }
+            for (String n : strArr) {
+                if (n.toLowerCase().startsWith("ь")) {
+                    System.out.print(n + " ");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("The entered data is incorrect");
         }
-        return sum;
-    }
-
-
-    public static float sumMassRec(float[] arr, int n){
-        if (n <= 0){
-            return 0;
-        }
-        n--;
-        if (arr[n] > 0){
-            return arr[n] + sumMassRec(arr, n);
-        } else {
-            return sumMassRec(arr, n);
-        }
-    }
-
-    public static float sumMassRec(float[] arr){
-        return sumMassRec(arr, arr.length);
-    }
-
-
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите размер массива");
-        int n = scanner.nextInt();
-
-        float[] mass = new float[n];
-        for (int i = 0; i < n; i++){
-            System.out.println("Введите " + (i + 1) + "-й єдемент");
-            mass[i] = scanner.nextFloat();
-        }
-        System.out.println(sumMassRec(mass));
     }
 }
