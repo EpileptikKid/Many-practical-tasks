59a3e32c478da5924d9c3e3371c368ead2f4299a
Andrii <107118319+EpileptikKid@users.noreply.github.com>
2022-09-17 19:51:14 +0300

    Update task_4_11.java

--- task_4_11.java
+++ task_4_11.java
@@ -1,44 +1,46 @@
-//Проинициализировать одномерный массив целых чисел случайными значениями
-//Осуществить упорядочение массива методом сортировки выбором. Алгоритм заключается в следующем:
-//находим номер минимального значения в текущем списке;
-//производим обмен этого значения со значением первой неотсортированной позиции (обмен не нужен, если минимальный элемент уже находится на данной позиции);
-//сортируем хвост списка, исключив из рассмотрения уже отсортированные элементы.
-//Результат вывести на экран.
+//Initialize a one-dimensional array of integers with random values
+//Sort an array using selection sort. The algorithm is as follows:
+//find the number of the minimum value in the current list;
+//we exchange this value with the value of the first unsorted position (the exchange is not needed if the minimum element is already at this position);
+//sort the tail of the list, excluding already sorted elements from consideration.
+//Display the result on the screen.
 
 import java.util.Scanner;
 
 public class task_4_11 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите размер массива:");
-        int sizeArr = scanner.nextInt();
-        int[] arr = new int[sizeArr];
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the size of the array:");
+            int sizeArray = scanner.nextInt();
+            int[] arr = new int[sizeArray];
 
-        for (int i = 0; i < sizeArr; i++){
-            arr[i] = (int) (Math.random()*200-100);
-        }
+            System.out.println("Array:");
+            for (int i = 0; i < sizeArray; i++) {
+                arr[i] = (int) (Math.random() * 200 - 100);
+                System.out.print(arr[i] + " ");
+            }
 
-        for (int n : arr){
-            System.out.print(n + " ");
-        }
-        System.out.println("\nОтсортированньій массив:");
-
-        for (int i = 0; i < sizeArr-1; i++){
-            int min_index = i;
-            for (int j = i+1; j < sizeArr; j++){
-                if (arr[j] < arr[min_index]){
-                    min_index = j;
+            System.out.println("\nSorted array:");
+            for (int i = 0; i < sizeArray - 1; i++) {
+                int min_index = i;
+                for (int j = i + 1; j < sizeArray; j++) {
+                    if (arr[j] < arr[min_index]) {
+                        min_index = j;
+                    }
+                }
+                if (min_index != i) {
+                    int extraVar = arr[min_index];
+                    arr[min_index] = arr[i];
+                    arr[i] = extraVar;
                 }
             }
-            if (min_index != i) {
-                int extraVar = arr[min_index];
-                arr[min_index] = arr[i];
-                arr[i] = extraVar;
-            }
-        }
 
-        for (int n : arr){
-            System.out.print(n + " ");
+            for (int n : arr) {
+                System.out.print(n + " ");
+            }
+        } catch (Exception e) {
+            System.out.println("The entered data is incorrect");
         }
     }
 }
